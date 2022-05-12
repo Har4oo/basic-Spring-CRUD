7e383ca0c20a593dcdf5258ff6f58967e0b3dfeb
HAH1SF <hari.haralampiev@bosch.io>
2022-05-12 17:10:24 +0300

    initial commit

---
+++ src/main/java/com/example/springTutorial/user/UserController.java
@@ -0,0 +1,26 @@
+package com.example.springTutorial.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired private UserService service;
+
+    @GetMapping("/users")
+    public String showUserList(Model model){
+        List<User> listUsers = service.listAll();
+        model.addAttribute("listUsers",listUsers);
+
+        return "users";
+    }
+    @GetMapping("/user/new")
+    public String showNewForm(Model model){
+        model.addAttribute("user", new User());
+        return "user_form";
+    }
+}
---
+++ src/test/java/com/example/springTutorial/UserRepositoryTests.java
@@ -0,0 +1,33 @@
+package com.example.springTutorial;
+
+import com.example.springTutorial.user.User;
+import com.example.springTutorial.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew(){
+        User user = new User();
+
+        user.setEmail("random.email@gmail.com");
+        user.setPassword("12345678");
+        user.setFirstName("Random");
+        user.setLastName("Modnar");
+
+        User savedUser = repo.save(user);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+}
